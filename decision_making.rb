a90da0ac9d24e05a0d10be20953df9f62f3f1ef7
Wesley Miranda <wesleywes42@gmail.com>
2022-04-20 23:05:57 -0600

    Finish Work

--- decision_making.rb
+++ decision_making.rb
@@ -3,7 +3,7 @@
 # Spend some time changing the variables and running the file to see how the story changes.
 
 
-door_choice = 1
+door_choice = 3
 bear_clothing = ""
 bear_choice = 1
 
@@ -35,9 +35,23 @@ end
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+#line line 12 though 16 what is happening is it is getting the value from the variable and imputing
+#it as a elsif statement if they pick 1 it will pick hat then end the if statement if
+#this is not true then it will pick scarf.
+
 # 2. What variable has a new value assigned to it after the first if statement executes?
+#The variable that has a new value assigned is bear_clothing as it will change as
+# the user picks 1 or 2.
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+#If the value was to be 3 then it will still pick scarf because in the if statement it states that
+#if it is 1 then do hat but if it is not one it is scarf. this is because their is no restrictions on it.
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+#this a if elseif statement and this code is having them pick a bear from 1-3 and the code funtions
+#by the users decision. If user picked 1 it will print (x) and end the block of code but if picked 2
+#then it will skip one and print the 2ed line of code.
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+#it will print "You run as fast as you can into the next room. It's full of snakes!"
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+#You tell the bear the hat is too small and it starts to cry!"
 # 7. What is your favorite ending?
+#You stay with the bear and become it's best friend!"
--- self_eval.md
+++ self_eval.md
@@ -1,8 +1,13 @@
 # Self Evaluation
 
 1. How confident do you feel in your understanding and fluency with the comparison operators?
+Yes i feel really confident with comparison operators.
 1. How confident do you feel in your understanding and fluency with the logical operators?
+Yes i feel really confident with logical operators.
 1. How confident do you feel in your understanding and fluency with the if statements?
+I feel confident but i do not know why my code will not work for the pizza one it seems that something may be wrong but i do not know.
 1. How much time did you spend on this lesson/topic? How did that line up with your time estimate?
-1. What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that?
+i did spend a quit bit of time but i dont know since i did lose power.
+1. What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that? If my code is wrong i will still want to practice if statements.
 1. What questions do you still have? How will you get the answers you need?
+i have a questions for the pizza one and i will get my answer in a question tomorrow. 
